f80c41e9e00b25f26ad0d54c890b815f7dc12aa5
Flore <122017704+Flore72@users.noreply.github.com>
2024-08-08 20:59:30 +0200

    devoir

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector ("form");
+const prenom = document.getElementById ("prenom");
+const nom = document.getElementById ("nom");
+const mail = document.getElementById ("mail");
+const telephone = document.getElementById ("telephone");
+const msg = document.getElementById ("msg");
+const msgError= document.getElementById("msgError");
+
